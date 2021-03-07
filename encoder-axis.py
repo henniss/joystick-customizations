60e9fc4bdc8848b5078bfe48234d204124e8e2e9
Harris Enniss <henniss@google.com>
2021-03-07 14:39:54 -0800

    Minimal encoder-to-axis implementation

--- encoder-axis.py
+++ encoder-axis.py
@@ -1,61 +1,62 @@
-import math
-import sys
-
 import gremlin
+import math
 from gremlin.user_plugin import *
 from gremlin.util import log
 
+from time import sleep
 
+
+steps = IntegerVariable(
+    'steps',
+    'Number of encoder-ticks corresponding to full motion of the axis.',
+    initial_value=20,
+    min_value=1,
+    max_value=128)
+key_up = PhysicalInputVariable(
+'Up',
+'Encoder direction corresponding to an increase in the axis.',
+[gremlin.common.InputType.JoystickButton],
+)
+key_down = PhysicalInputVariable(
+'Down',
+'Encoder direction corresponding to a decrease in the axis.',
+[gremlin.common.InputType.JoystickButton],
+)
+v_axis = VirtualInputVariable(
+'axis',
+'vJoy axis to manipulate.',
+[gremlin.common.InputType.JoystickAxis],
+)
 
 mode = ModeVariable(
         "Mode",
         "The mode to use for this mapping"
 )
 
-N = IntegerVariable(
-    'N',
-    'Number of ticks on an encoder which should map to the full axis range.'
-    initial_value=10,
-    min_value=2,
-    max_value=sys.maxint)
-    
-up=PhysicalInputVariable(
-    'Up',
-    'Encoder direction which increments the axis',
-    [gremlin.common.InputType.JoystickButton]
-)
-down=PhysicalInputVariable(
-    'Down',
-    'Encoder direction which decrements the axis',
-    [gremlin.common.InputType.JoystickButton]
-)
-vaxis=VirtualInputVariable(
-    "Axis",   
-    "vJoy axis to manipulate.",
-    [gremlin.common.InputType.JoystickAxis]
-)
-
 value = -1
-    
-up_decorator = up.create_decorator(mode.value)
-down_decorator = down.create_decorator(mode.value)
 
-@up_decorator.axis(up.input_id)
-def axis_change(event, vjoy):
+up_decorator = key_up.create_decorator(mode.value)
+down_decorator = key_down.create_decorator(mode.value)
+
+
+@up_decorator.button(key_up.input_id)
+def up(event, vjoy):
+   global value
+   log("up")
+   if event.is_pressed:
+       value = min(1, value + 2/steps.value)
+       set_axis(vjoy, value)
+       log(f"set: {value}")
+
+@down_decorator.button(key_down.input_id)
+def down(event, vjoy):
     global value
     if event.is_pressed:
-        value = min(value + 2/N.value, 1)
-    setaxis(vjoy, value)
-
-@down_decorator.axis(down.input_id)
-def axis_change(event, vjoy):
-    global value
-    if event.is_pressed:
-        value = max(value - 2/N.value, -1)
-    setaxis(vjoy, value)
-
-def setaxis(vjoy, value):
-    device = vjoy[vaxis.value['device_id']]
-    axis = device.axis(vaxis.value['input_id'])
-    axis.value = value
-    
+        value = max(-1, value - 2/ steps.value)
+        set_axis(vjoy, value)
+        log(f"set: {value}")
+        
+def set_axis(vjoy, x):
+    device = vjoy[v_axis.value['device_id']]
+    axis= device.axis(v_axis.value['input_id'])
+    axis.value = x
