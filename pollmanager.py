830aa460aa8a98114f77cdda0ff8425b5ae3a24a
Harris Enniss <henniss@google.com>
2021-03-29 08:17:55 -0700

    Add license headers everywhere.

--- pollmanager.py
+++ pollmanager.py
@@ -1,3 +1,18 @@
+#Copyright 2021 Google LLC
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 
 from gremlin.util import log
 
@@ -6,14 +21,14 @@ callbacks = []
 def register_vjoy_callback(fn):
     global callbacks
     callbacks.append(fn)
-    
-    
+
+
 def do_callbacks(vjoy):
     global callbacks
     for cb in callbacks:
         cb(vjoy)
-        
-        
+
+
 @register_vjoy_callback
 def do_log(vjoy):
     log("Callbacks started")
--- toggle-macro.py
+++ toggle-macro.py
@@ -1,3 +1,17 @@
+#Copyright 2021 Google LLC
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import gremlin
 import math
 from gremlin.user_plugin import *
@@ -33,7 +47,7 @@ toggle_decorator = toggle.create_decorator(mode.value)
 def flip(event, vjoy):
    off = vjoy[v_off.value['device_id']].button(v_off.value['input_id'])
    on = vjoy[v_on.value['device_id']].button(v_on.value['input_id'])
-   
+
    if event.is_pressed:
        on.is_pressed = True
        sleep(0.1)
